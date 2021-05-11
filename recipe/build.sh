b82919efef46a2f2d72f214b3bde3a841d7c8a06
Iain Russell <40060766+iainrussell@users.noreply.github.com>
2021-05-11 10:41:55 +0100

    Test 3 of 4.7.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -22,7 +22,8 @@ cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DREPLACE_TPL_ABSOLUTE_PATHS=$REPLACE_TPL_ABSOLUTE_PATHS \
       $SRC_DIR
 
-make -j $CPU_COUNT
+#make -j $CPU_COUNT
+make VERBOSE=1
 
 ctest --output-on-failure -j $CPU_COUNT
 make install
